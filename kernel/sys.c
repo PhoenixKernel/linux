70fefef7fd0bc6984f00d3af827dd95faa500eec
Tim Zimmermann <tim@linux4.de>
2024-09-13 07:47:57 +0200

    Revert "syscall: Only fake uname on very first call of netbpfload"
    This reverts commit 6c8e37078ff8e03f2b839c85fcf3801e324ddc3b.
    
    Reason for revert: Google did something that caches the kernel version now even for bpf prog loading
    
    Change-Id: I61f21453d326bbcd42fdc6d012d92b47d2056944

--- kernel/sys.c
+++ kernel/sys.c
@@ -1182,17 +1182,15 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-
-static uint64_t netbpfload_pid = 0;
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "netbpfload", 10) &&
-	    current->pid != netbpfload_pid) {
-		netbpfload_pid = current->pid;
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4)) {
 		strcpy(tmp.release, "6.6.40");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
